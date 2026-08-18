25d7c6a029c8c98ef1613e05a03612e0df2bcd64
snekkerlien <mads@snekkerlien.no>
2026-08-18 02:05:47 +0200

    bugfix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function LoginPage() {
   const router = useRouter();
-
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
