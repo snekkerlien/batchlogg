6e3eb5e3424442aae3c9f4b321f167fb8ec13db3
snekkerlien <mads@snekkerlien.no>
2026-08-18 18:30:07 +0200

    bugfix

--- app/auth/signup/signupAction.ts
+++ app/auth/signup/signupAction.ts
@@ -1,28 +1,16 @@
-"use server";
-
-import { supabaseServerClient } from "../../../lib/supabaseServerClient";
+import { supabase } from "../../../lib/supabaseClient";
 
 export async function signupAction(username: string, password: string) {
-  console.log("SIGNUP ACTION STARTED");
-
-  const supabase = await supabaseServerClient();
   const email = `${username}@fake.local`;
-
-  console.log("EMAIL:", email);
 
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
   });
 
-  console.log("SUPABASE RESPONSE:", data, error);
-
   if (error) {
-    console.error("SUPABASE ERROR:", error);
     throw new Error(error.message);
   }
 
-  console.log("SIGNUP SUCCESS:", data);
-
-  return { user: data.user };
+  return data;
 }
