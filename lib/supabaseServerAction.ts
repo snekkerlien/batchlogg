c81e3832f2c2e8731a93a3be976712cf02d457a5
snekkerlien <mads@snekkerlien.no>
2026-08-18 21:08:56 +0200

    bugfix

--- lib/supabaseServerAction.ts
+++ lib/supabaseServerAction.ts
@@ -1,10 +1,10 @@
 import { cookies } from "next/headers";
-import { createServerClient as createSupabaseClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 
-export async function createServerClient() {
-  const cookieStore = await cookies(); // hos deg er cookies() async
+export async function createServerActionClient() {
+  const cookieStore = await cookies();
 
-  return createSupabaseClient(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -13,10 +13,10 @@ export async function createServerClient() {
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: any) {
-          cookieStore.set(name, value, options); // FUNKSJON-signatur
+          cookieStore.set(name, value, options);
         },
         remove(name: string, options: any) {
-          cookieStore.set(name, "", options); // FUNKSJON-signatur
+          cookieStore.set(name, "", options);
         },
       },
     }
