5c9e125f1e57254549c7019923ead4f9a069290b
snekkerlien <mads@snekkerlien.no>
2026-08-18 15:44:40 +0200

    bugfix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signupAction } from "../(auth)/signup/signupAction";
 
+
 export default function SignupPage() {
   const router = useRouter();
 
