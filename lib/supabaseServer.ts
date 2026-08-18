085229af35de8d9204111f4305571802a1ce657d
snekkerlien <mads@snekkerlien.no>
2026-08-18 15:58:30 +0200

    bugfix

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -6,7 +6,7 @@ export async function supabaseServer() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!, // ⭐ DENNE ER NY
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
