39972819f46ee3bf06ccb32ef8217318d2b4b5b8
snekkerlien <mads@snekkerlien.no>
2026-08-20 23:03:11 +0200

    fikser login skjerm mens innlogget

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
-"use client";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
